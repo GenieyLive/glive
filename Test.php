de8053ad174cad773c3e2c5717b0ee5198edc391
Palaninathan <406649@cognizant.com>
2017-04-04 17:26:56 +0530

    added mail template

--- Test.php
+++ Test.php
@@ -1,8 +1,14 @@
 <?php
 echo date('D M d Y H:i:s T O');
 $row['type']="1";
+$device_id="lol";
 $res=$row['type']==1?"admin":"user";
 echo $res;
 require('etc/mail/mail.php');
-sendMail("naveen.ccmsd@gmail.com","Test Subject","Complete test mail");
+$s = file_get_contents('etc/mail/action.html');
+$s = str_replace('$device_id', 'lol', $s);
+// sendMail("naveen.ccmsd@gmail.com","Test Subject",$s);
+echo $s;
+// echo sprintf($s, $device_id);
+
 ?>
