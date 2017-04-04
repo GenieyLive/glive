7526d0708178fff47c6005a6471b83237a687c23
Palaninathan <406649@cognizant.com>
2017-04-04 16:19:43 +0530

    added mail template

--- Test.php
+++ Test.php
@@ -1,6 +1,8 @@
 <?php
 echo date('D M d Y H:i:s T O');
 $row['type']="1";
-$res=$row['type']==1?"admmin":"user";
+$res=$row['type']==1?"admin":"user";
 echo $res;
+require('etc/mail/mail.php');
+sendMail("naveen.ccmsd@gmail.com","Test Subject","Complete test mail");
 ?>
